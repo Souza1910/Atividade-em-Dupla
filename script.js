a61309a8d93efee635ae33af3f2a863d755773ac
Souza1910 <Leandrocruzsousa199@gmail.com>
2023-05-07 10:27:22 -0300

    2att
    2°

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let valor = 10
+const textos = ['Imagem 1', 'Imagem 2', 'Imagem 3']
 
-function incrementa() {
-  console.log('Estou dentro da função!')
+const imagens = ['img1.png', 'img2.png', 'img3.png']
+
+const divImagem = document.getElementById('imagem')
+
+function selecionaImagem() {
+  const opcao = Math.floor(Math.random() * imagens.length)
+  divImagem.style.backgroundImage = url('imagens/${imagens[opcao]}')
 }
-incrementa()
-console.log(valor)
+
+setInterval(selecionaImagem, 3000)
+
+const paragrafo = document.getElementById('texto')
+
+let indice = 0
+
+function trocaTexto() {
+  paragrafo.innerText = textos[indice]
+  indice++
+  if (indice === textos.length) {
+    indice = 0
+  }
+}
+
+setInterval(trocaTexto, 2000)
