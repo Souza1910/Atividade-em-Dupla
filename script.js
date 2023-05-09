1c5e28e5e5837eec43e97e3da35ef8f1bcd11b13
Souza1910 <Leandrocruzsousa199@gmail.com>
2023-05-09 08:59:26 -0300

    eee
    eee

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function carregar(){
+  var msg= window.document.getElementById('msg')
+  var imgs = window.document.getElementById('img')
+  msg.innerHTML = 'edrtuyedtudtd'
+imgs.src = 'img/img3.png'
 
+}
