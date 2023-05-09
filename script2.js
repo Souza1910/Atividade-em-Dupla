a615208603a6aecebcd9783bc17b153ac35e1e81
Souza1910 <130998067+Souza1910@users.noreply.github.com>
2023-05-09 09:12:03 -0300

    aaaaaaaaaaaaaaaa
    aaaaaaaaaaaaa

---
+++ script2.js
@@ -0,0 +1,10 @@
+function batata(){
+    var msg = window.document.querySelector('#text')
+      msg.innerHTML = 'janif programadora'
+
+      var img = window.document.querySelector('#imagem')
+      img.src = "img2.png"
+
+
+
+}
